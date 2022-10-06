d3f58b572ec37fa8765ac13d2bda3c29fc9af4b5
Yarik <36842297+yaroGOR@users.noreply.github.com>
2022-10-06 21:31:17 +0300

    Bug fixed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import './App.css';
+import "./styles.css";
 
-function App() {
+import React from 'react';
+import { Canvas } from '@react-three/fiber';
+import { Environment, OrbitControls } from "@react-three/drei";
+import { Suspense } from "react";
+import Model from "./Model";
+
+export default function App() {
   return (
     <div className="App">
-      <p>Hi netify</p>
+      <Canvas>
+        <Suspense fallback={null}>
+          <Model />
+          <OrbitControls />
+          <Environment preset="sunset" background />
+        </Suspense>
+      </Canvas>
     </div>
   );
-}
-
-export default App;
+};
